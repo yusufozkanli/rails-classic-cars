2f876ec70dc7302506c22d4d644ef79dfa9dc6f2
yusuf ozkanli <myozkanli@gmail.com>
2017-11-13 14:32:48 +0000

    models updated

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,4 @@
 class Car < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
+  has_many :rentals
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,4 @@
 class Rental < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :car_id
+  belongs_to :user
+  belongs_to :car
 end
