1b075de3a42fd23489f3495c7c2c4615a8708660
Ehsun Tafsiri <ehsuntafsiri@gmail.com>
2017-11-15 12:32:20 +0000

    Update done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -23,6 +23,16 @@ class CarsController < ApplicationController
     end
   end
 
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
   private
 
   def car_params
