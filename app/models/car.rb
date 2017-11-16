b0b911cc14cb3a43fcc9f9697e52e52e924070f1
Michel Lieben <liebenmichel@gmail.com>
2017-11-16 12:55:31 +0000

    added validators

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ApplicationRecord
   pg_search_scope :search, against: [ :brand, :model, :year, :color, :price ]
   belongs_to :user
   has_many :rentals
+  validates :brand, :model, :year, :color, :price, presence: true
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
