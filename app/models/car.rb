c9fb045ab13a983043669f712e60ea777303efa5
Ehsun Tafsiri <ehsuntafsiri@gmail.com>
2017-11-16 11:18:46 +0000

    done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
