11242fbf66649784c8a0c3756e773f8a77efaabf
Michel Lieben <liebenmichel@gmail.com>
2017-11-17 11:33:06 +0000

    add review to car

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @car = Car.find(params[:car_id])
+    @review = Review.new(review_params)
+    @review.car = @car
+    if @review.save
+      redirect_to car_path(@car)
+    else
+      render 'cars/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
