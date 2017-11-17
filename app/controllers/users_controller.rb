e741a9380a94446db5980451e71776ec0311c9fc
yusuf ozkanli <myozkanli@gmail.com>
2017-11-17 12:25:05 +0000

    added user profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def user_params
+  params.require(:user).permit(:name, :phone, :email, :github_username)
+  end
+
 end
